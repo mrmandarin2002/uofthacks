90be92899fcfdd8d990691479790b775b51a91c5
Derek Ma <46353341+mrmandarin2002@users.noreply.github.com>
2021-02-19 21:52:24 -0600

    ok

---
+++ Server Shit/server.py
@@ -0,0 +1,37 @@
+import socket
+from datetime import datetime
+
+import client_thread
+
+PORT = 5055
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+HEADER = 16
+
+# function to get the current time
+def get_time():
+    return str(datetime.now()).split()[1].split(".")[0]+" "
+
+class server():
+
+    def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket
+        self.ip_address = socket.gethostbyname(socket.gethostname())
+        self.server_address = (self.ip_address, PORT)
+        self.server.bind(self.server_address)
+        self.client_list = []
+        self.server_loop()
+
+    def server_loop(self):
+        # main server loop
+        print(get_time()+"Starting main server loop...")
+        print("Please connect to", self.ip_address)
+        self.server.listen()
+        while True:
+            # receive incoming packages
+            conn, addr = self.server.accept()
+            self.client_list.append(client_thread.client_thread(conn, addr, self))
+            print(get_time() + f" New client connected. There are currently {threading.activeCount() - 1} client(s) connected!")
+
+if __name__ == "__main__":
+    s = server()
---
+++ shoppingdashboardv2/app/src/main/java/com/example/shoppingdashboardv2/interactions.java
@@ -0,0 +1,26 @@
+package com.example.shoppingdashboardv2;
+
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+
+public class interactions {
+
+    int PORT = 5055;
+    String FORMAT = "utf-8";
+    String DISCONNECT_MESSAGE = "!DISCONNECT";
+    int HEADER = 16;
+    String server_ip = "192.168.100.110";
+
+    public interactions() throws IOException {
+        Socket tcp_socket = new Socket(server_ip, PORT);
+        PrintWriter out = new PrintWriter(tcp_socket.getOutputStream(), true);
+        out.println("TESTING_BITCHES");
+    }
+
+    public void send(String message){
+
+    }
+
+}
