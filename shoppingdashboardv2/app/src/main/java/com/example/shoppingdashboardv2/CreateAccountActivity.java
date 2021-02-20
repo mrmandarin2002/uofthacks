f8adbdfcc34fae4447eedc02ecd95a1a22f06903
Akhil Ambardekar <42975101+AkhilAmbardekar@users.noreply.github.com>
2021-02-19 23:25:30 -0500

    created a new dashboard activity and layout

--- shoppingdashboardv2/app/src/main/java/com/example/shoppingdashboardv2/CreateAccountActivity.java
+++ shoppingdashboardv2/app/src/main/java/com/example/shoppingdashboardv2/CreateAccountActivity.java
@@ -3,12 +3,26 @@ package com.example.shoppingdashboardv2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    private Button createActBTN;
+    private TextView retLoginTV;
+    private EditText emailET;
+    private EditText passwordET;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        createActBTN = findViewById(R.id.createActBTN);
+        retLoginTV = findViewById(R.id.retLoginTV);
+        emailET = findViewById(R.id.emailET);
+        passwordET = findViewById(R.id.passwordET);
     }
 }
